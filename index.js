057d6a06106edcf10f3b29fba3327ec2801082de
vimelon <vimelon131@gmail.com>
2021-06-03 12:38:10 +0700

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 console.log("Halo");
 import fs from "fs";
 import _ from "lodash";
-console.log(fs.readFileSync("test.js","utf-8"));
-console.log(_.intersection([1,2],[2,3]));
+console.log(fs.readFileSync("test.js", "utf-8"));
+console.log(_.intersection([1, 2], [2, 3]));
+
+const fn = (a, b) => {
+  const c = a + b;
+  return c;
+};
